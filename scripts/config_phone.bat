c0ab44bf400d25f5068ba4359ac903f560c48bc2
eric.chan <chenyijin1983@gmail.com>
2015-03-03 11:17:53 +0800

    Code refactor for the config_phone.bat

--- scripts/config_phone.bat
+++ scripts/config_phone.bat
@@ -1,5 +1,2 @@
-set ADB_PATH=D:\tools\Android\android-sdk\platform-tools
-set USER_DATA_PATH=D:\projects\private\crazy-monkey\userdata
-
-%ADB_PATH%\adb -s %1 push %USER_DATA_PATH%\xposeDevice.txt /mnt/sdcard/.system
-%ADB_PATH%\adb -s %1 shell am instrument -w com.crazymonkey.test009/android.test.InstrumentationTestRunner
+%ANDROID_SDK_HOME%\platform-tools\adb -s %1 push %CRAZY_MONKEY_HOME%\userdata\xposeDevice.txt /mnt/sdcard/.system
+%ANDROID_SDK_HOME%\platform-tools\adb -s %1 shell am instrument -w com.crazymonkey.test009/android.test.InstrumentationTestRunner
