f40078dfc6739777c630eda44a551782772ce0a5
eric.chan <chenyijin1983@gmail.com>
2015-03-05 15:33:03 +0800

    change the run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-export JAVA_HOME=/home/ericchen/crazy-monkey/jdk1.7.0_75
+export USER_HOME=/home/ericchen
+export JAVA_HOME=$USER_HOME/crazy-monkey/jdk1.7.0_75
 export JRE_HOME=$JAVA_HOME/jre
-export ANDROID_SDK_HOME=/home/ericchen/crazy-monkey/android-sdk-linux
-export ANT_HOME=/home/ericchen/crazy-monkey/apache-ant-1.9.4
 export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
-export CRAZY_MONKEY_HOME=/home/ericchen/crazy-monkey/crazy-monkey
+export ANT_HOME=$USER_HOME/crazy-monkey/apache-ant-1.9.4
+
+export ANDROID_SDK_HOME=$USER_HOME/crazy-monkey/android-sdk-linux
+export CRAZY_MONKEY_HOME=$USER_HOME/crazy-monkey/crazy-monkey
 
 export DISPLAY=:0.0
 export LD_LIBRARY_PATH=$ANDROID_SDK_HOME/tools/lib
 
 export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$ANDROID_SDK_HOME/tools:$ANDROID_SDK_HOME/platform-tools:$ANT_HOME/bin:$PATH
 
-cd /home/ericchen/crazy-monkey/crazy-monkey
-/home/ericchen/crazy-monkey/jdk1.7.0_75/bin/java -jar /home/ericchen/crazy-monkey/crazy-monkey/crazy-monkey-0.1.jar
+cd $CRAZY_MONKEY_HOME
+
+# Clean the env
+
+# Git update
+
+# Build 
+$ANT_HOME/bin/ant
+
+# Run VPN
+
+# Run the testing
+$JAVA_HOME/bin/java -jar $CRAZY_MONKEY_HOME/crazy-monkey-0.1.jar
+
+# Kill VPN
+
