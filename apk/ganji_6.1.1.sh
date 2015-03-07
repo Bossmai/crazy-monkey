9bc8b42e404bd6355a1a6220409e34d7d65efee6
eric.chan <chenyijin1983@gmail.com>
2015-03-07 13:31:43 +0800

    Change the enter char of the sh file to Linux

--- apk/ganji_6.1.1.sh
+++ apk/ganji_6.1.1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$ANDROID_SDK_HOME/platform-tools/adb -s $1 install $CRAZY_MONKEY_HOME/apk/ganji_6.1.1_test.apk
+$ANDROID_SDK_HOME/platform-tools/adb -s $1 install $CRAZY_MONKEY_HOME/apk/ganji_6.1.1_test.apk 
 $ANDROID_SDK_HOME/platform-tools/adb -s $1 shell am instrument -w com.crazyroba/android.test.InstrumentationTestRunner
