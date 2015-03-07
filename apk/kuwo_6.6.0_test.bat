b2042f5cb31cf69ddae0f73f7785ccdb0570a2c4
eric.chan <chenyijin1983@gmail.com>
2015-03-07 13:27:35 +0800

    Remove the kuwo

--- apk/kuwo_6.6.0_test.bat
+++
@@ -1,2 +0,0 @@
-%ANDROID_SDK_HOME%\platform-tools\adb -s %1 install %CRAZY_MONKEY_HOME%\apk\kuwo_6.6.0_test.apk
-%ANDROID_SDK_HOME%\platform-tools\adb -s %1 shell am instrument -w com.crazyroba/android.test.InstrumentationTestRunner
