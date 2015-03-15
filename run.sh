774816b7a06251f16e316bce01e7d325dbdf56c6
eric.chan <chenyijin1983@gmail.com>
2015-03-15 21:42:09 +0800

    add stop.sh and setenv.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
-# setup the git
-
-# set the env
-export USER_HOME=/home/ericchen
-export JAVA_HOME=$USER_HOME/crazy-monkey/jdk1.7.0_75
-export JRE_HOME=$JAVA_HOME/jre
-export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
-export ANT_HOME=$USER_HOME/crazy-monkey/apache-ant-1.9.4
-
-export VPN_CLINET_HOME=$USER_HOME/crazy-monkey/vpn-client
-export ANDROID_SDK_HOME=$USER_HOME/crazy-monkey/android-sdk-linux
-export CRAZY_MONKEY_HOME=$USER_HOME/crazy-monkey/crazy-monkey
-
-export DISPLAY=:0.0
-export LD_LIBRARY_PATH=$ANDROID_SDK_HOME/tools/lib
-
-export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$ANDROID_SDK_HOME/tools:$ANDROID_SDK_HOME/platform-tools:$ANT_HOME/bin:$PATH
+# clean the evn
+/bin/bash ./stop.sh
 
 # run the vpn
 cd $VPN_CLINET_HOME
 git pull
 $VPN_CLINET_HOME/autorun
-
-# Kill the emulator process
-ps aux | grep emulator | awk '{print $2}' | xargs kill -9
-# Clean the lock file of avd
-ls -l $ANDROID_SDK_HOME/.android/avd | grep "\.avd" | awk '{print $9}' | xargs rm -rf *.lock
 
 # Git update and build
 cd $CRAZY_MONKEY_HOME
--- stop.sh
+++ stop.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-export USER_HOME=/home/ericchen
-export ANDROID_SDK_HOME=$USER_HOME/crazy-monkey/android-sdk-linux
+# set the env
+/bin/bash ./setenv.sh
+
+# Kill the adb
+echo "Kill the adb server"
+pgrep adb | xargs kill -9
 
 # Kill the emulator process
-ps aux | grep emulator | awk '{print $2}' | xargs kill -9
+echo "Kill the emulator processes"
+ps aux | grep emulator | awk '{print $2}' | xargs kill -9 
+
 # Clean the lock file of avd
+echo "Cleaning the locked avd files"
+cd $ANDROID_SDK_HOME/.android/avd
 ls -l $ANDROID_SDK_HOME/.android/avd | grep "\.avd" | awk '{print $9}' | xargs rm -rf *.lock
+
+# Kill the java
+echo "Kill the crazy monkey java process"
+ps aux | grep "bin/java -jar $CRAZY_MONKEY_HOME/crazy-monkey-0.1.jar" | awk '{print $2}' | xargs sudo kill -9
+
+# Kill the vpn-daemon
+echo "Kill the VPN daemon"
+pgrep vpn-daemon | xargs sudo kill -9 
+
+# Kill the main
+echo "Kill the main sh"
+ps aux | grep main | awk '{print $2}' | xargs kill -9
+
+# Kill the pptp
+echo "Kill the pptp processes"
+ps aux | grep pptp | awk '{print $2}' | xargs sudo kill -9 
+
+# Restart the net interface
+echo "Restart the net interface"
+sudo ifconfig $NETWORK_INTERFACE down
+sudo ifconfig $NETWORK_INTERFACE up
