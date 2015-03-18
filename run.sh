d9a105e331b0e0b1927624427920d27c5bc1d331
eric.chan <chenyijin1983@gmail.com>
2015-03-19 01:40:08 +0800

    change the order of script

--- run.sh
+++ run.sh
@@ -7,14 +7,15 @@ source ./setenv.sh
 echo "Update the crazy monkey code..."
 cd $CRAZY_MONKEY_HOME && git pull
 
+echo "Build the project"
+cd $CRAZY_MONKEY_HOME && $ANT_HOME/bin/ant
+
+# Clean the env
 cd $CRAZY_MONKEY_HOME && /bin/bash ./stop.sh
 
 echo "Run the vpn client"
 cd $CRAZY_MONKEY_HOME && /bin/bash ./reset_vpn.sh
 cd $VPN_CLINET_HOME && ./autorun
-
-echo "Build the project"
-cd $CRAZY_MONKEY_HOME && $ANT_HOME/bin/ant
 
 # Run the testing
 $JAVA_HOME/bin/java -jar $CRAZY_MONKEY_HOME/crazy-monkey-0.1.jar
