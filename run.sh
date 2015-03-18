1f91e436a005d00bb753e85e28ff2a29f246ea77
eric.chan <chenyijin1983@gmail.com>
2015-03-19 01:34:26 +0800

    Fix the scripts

--- run.sh
+++ run.sh
@@ -3,15 +3,15 @@
 # clean the evn
 source ./setenv.sh
 
+# Git update and build
+echo "Update the crazy monkey code..."
+cd $CRAZY_MONKEY_HOME && git pull
+
 cd $CRAZY_MONKEY_HOME && /bin/bash ./stop.sh
 
 echo "Run the vpn client"
 cd $CRAZY_MONKEY_HOME && /bin/bash ./reset_vpn.sh
 cd $VPN_CLINET_HOME && ./autorun
-
-# Git update and build
-echo "Update the crazy monkey code..."
-cd $CRAZY_MONKEY_HOME && git pull
 
 echo "Build the project"
 cd $CRAZY_MONKEY_HOME && $ANT_HOME/bin/ant
