e8037a80e18be7ce78d86ed3b5b99753d9d4db25
eric.chan <chenyijin1983@gmail.com>
2015-03-19 14:50:02 +0800

    Add remove modem file into scripts

--- stop.sh
+++ stop.sh
@@ -3,28 +3,30 @@
 # set the env
 source ./setenv.sh
 
-echo "----- Clean the enviroment -----"
+echo "-------------------- Clean the enviroment --------------------"
 # Kill the adb
-echo "Kill the adb server..."
+echo "[Android Emulator] Kill the adb server..."
 pgrep adb | xargs -rt kill -9
 
 # Kill the emulator process
-echo "Kill the emulator processes..."
+echo "[Android Emulator] Kill the emulator processes..."
 pgrep emulator64-arm | xargs -rt kill -9
 
 # Clean the lock file of avd
-echo "Clean the locked avd files..."
+echo "[Android Emulator] Clean the locked avd files..."
 cd $ANDROID_SDK_HOME/.android/avd && find $ANDROID_SDK_HOME/.android/avd -name *.lock | xargs -rt rm -rf
+echo "[Android Emulator] Clean the modem files..."
+cd $ANDROID_SDK_HOME/.android && rm modem-nv-ram-*
 
 # Kill the java
-echo "Kill the crazy monkey java process..."
+echo "[Crazy Monkey] Kill the crazy monkey java process..."
 ps aux | grep "bin/java -jar $CRAZY_MONKEY_HOME/crazy-monkey-0.1.jar" | awk '{print $2}' | xargs -rt sudo kill -9
 
 # Tar the log files
-echo "Compress all the log files..."
+echo "[Crazy Monkey] Compress all the log files..."
 cd $CRAZY_MONKEY_HOME/logs && tar zcvf ./log_`date '+%Y-%m-%dT%H:%M:%S'`.zip ./*.log
 
-echo "Remove the log files..."
+echo "[Crazy Monkey] Remove the log files..."
 cd $CRAZY_MONKEY_HOME/logs && rm *.log 
 
-echo "----- Done -----"
+echo "-------------------- Done --------------------"
